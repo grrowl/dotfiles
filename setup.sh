d9edaaa020eacc0194bd75480b5b604e19e4805e
Tom <tom@chillidonut.com>
2013-07-30 10:59:20 +1000

    Robust setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 # symlink dotfiles
-SCRIPT_PATH=`realpath $0`
-dotfiles=`dirname $SCRIPT_PATH`
+dir=~/dotfiles
 olddir=~/dotfiles_old   # backup directory
-
-
 files=".bashrc .vimrc .bash_logout .gitconfig"    # list of files/folders to symlink in homedir
 
 ## create dotfiles_old in homedir
-echo "Creating $olddir for backup"
 mkdir -p $olddir
-cd $dotfiles
 
 ## move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
