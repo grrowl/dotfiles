32ecd3636c687343a785ab5ddd4653ed9c0129df
Tom McKenzie <tom@chillidonut.com>
2018-08-16 16:48:45 +1000

    Code install extensions instruction

--- Code/update.sh
+++ Code/update.sh
@@ -1,2 +1,5 @@
 grep -v 'oauth' /Users/tom/Library/Application\ Support/Code/User/settings.json > ./User/settings.json
 code --list-extensions > ./list-extensions.txt
+
+# install extensions using:
+# cat list-extensions.txt | while read line; do code --install-extension "$line"; done
