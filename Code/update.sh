1e7ed927d4133df069e6dd8049132aa935dd3023
Tom McKenzie <tom@chillidonut.com>
2019-04-29 13:42:03 +1000

    Update Code scripts

--- Code/update.sh
+++ Code/update.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
+set -e
+
 grep -v 'oauth' /Users/tom/Library/Application\ Support/Code/User/settings.json > ./User/settings.json
+cp /Users/tom/Library/Application\ Support/Code/User/keybindings.json ./User/
 code --list-extensions > ./list-extensions.txt
 
 # install extensions using:
