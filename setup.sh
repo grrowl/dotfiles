b708cc1abc77477e692086c256b0b591f4330d63
Tom McKenzie <tom@chillidonut.com>
2019-05-23 18:58:27 +1000

    Better setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # symlink dotfiles
-dir=~/dotfiles
-olddir=~/dotfiles/bak   # backup directory
+dir="$HOME/dotfiles"
+olddir="$HOME/dotfiles/bak"   # backup directory
 files=".zshrc .zshrc.d .rc.d .bashrc .vimrc .bash_logout .gitconfig .gitignore_global .gemrc"    # list of files/folders to symlink in homedir
 
 ## create dotfiles_old in homedir
@@ -8,7 +10,7 @@ mkdir -p $olddir
 
 ## move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    mv -f ~/$file $olddir
+    mv -f "$HOME/$file" $olddir
     echo "Creating symlink to $file in ~"
-    ln -s $dir/$file ~/$file
+    ln -s "$dir/$file" "$HOME/$file"
 done
