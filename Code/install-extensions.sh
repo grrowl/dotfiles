067d41ac3aab158f0ddc539de0e5d572754a47d4
Tom McKenzie <tom@chillidonut.com>
2019-06-06 11:34:15 +1000

    Update Code

--- Code/install-extensions.sh
+++ Code/install-extensions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-cat list-extensions.txt | while read line; do code --install-extension "$line"; done
+# uninstall extensions not in list-extensions.txt
+code --list-extensions | comm -1 -3 list-extensions.txt - | while read -r line; do code --uninstall-extension "$line"; done
+
+# install extensions not currently installed
+code --list-extensions | comm -2 -3 list-extensions.txt - | while read -r line; do code --install-extension "$line"; done
