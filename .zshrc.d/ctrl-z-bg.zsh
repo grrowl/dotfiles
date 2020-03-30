a9dcde276746916e86e782ec8296a5f2d662e920
Tom McKenzie <tom@chillidonut.com>
2020-03-30 12:47:42 +1100

    Adds ctrl+z at empty prompt resumes background job

---
+++ .zshrc.d/ctrl-z-bg.zsh
@@ -0,0 +1,13 @@
+# https://unix.stackexchange.com/a/223440
+
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    bg
+    zle redisplay
+  else
+    zle push-input
+  fi
+}
+
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
