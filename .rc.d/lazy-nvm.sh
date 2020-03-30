c5a4a3f1087011703db4f09b5e2734f01e77e6c1
Tom McKenzie <tom@chillidonut.com>
2020-03-30 12:48:09 +1100

    Adds yarn to lazy-nvm

--- .rc.d/lazy-nvm.sh
+++ .rc.d/lazy-nvm.sh
@@ -5,6 +5,7 @@ function lazy_nvm {
   unset -f npm
   unset -f node
   unset -f npx
+  unset -f yarn
 
   if [ -d "${HOME}/.nvm" ]; then
     export NVM_DIR="$HOME/.nvm"
@@ -23,3 +24,4 @@ function nvm { lazy_nvm; nvm "$@"; }
 function npm { lazy_nvm; npm "$@"; }
 function node { lazy_nvm; node "$@"; }
 function npx { lazy_nvm; npx "$@"; }
+function yarn { lazy_nvm; yarn "$@"; }
