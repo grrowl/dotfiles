d5e25296284640272111c4355f17bfd6e30984fd
Tom McKenzie <tom@chillidonut.com>
2020-05-20 20:08:32 +1000

    Add vim plugins gitgutter, editorconfig, todo

--- .vimrc
+++ .vimrc
@@ -11,7 +11,7 @@ set incsearch	" Searches for strings incrementally
 
 set autoindent	" Auto-indent new lines
 set expandtab	" Use spaces instead of tabs
-set shiftwidth=4	" Number of auto-indent spaces
+set shiftwidth=2	" Number of auto-indent spaces
 set smartindent	" Enable smart-indent
 set smarttab	" Enable smart-tabs
 set softtabstop=2	" Number of spaces per Tab
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # symlink dotfiles
 dir="$HOME/dotfiles"
 olddir="$HOME/dotfiles/bak"   # backup directory
-files=".zshrc .zshrc.d .rc.d .bashrc .vimrc .bash_logout .gitconfig .gitignore_global .gemrc"    # list of files/folders to symlink in homedir
+files=".zshrc .zshrc.d .rc.d .bashrc .vimrc .bash_logout .gitconfig .gitignore_global .gemrc .vim"    # list of files/folders to symlink in homedir
 
 ## create dotfiles_old in homedir
 mkdir -p $olddir
